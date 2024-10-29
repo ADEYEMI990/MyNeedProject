6019fd77acd47377912b630e83e6852f3b80d33c
Adeyemi <adeyemiibrahim990@gmail.com>
2024-10-29 05:24:35 -0700

    Creating the Login Page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
-import Add from './Pages/Add'
-import List from './Pages/List'
-import Orders from './Pages/Orders'
+import Add from "./Pages/Add";
+import List from "./Pages/List";
+import Orders from "./Pages/Orders";
+import Login from "./components/Login";
 
 const App = () => {
+  const [token, setToken] = useState("");
+
   return (
     <div className= "bg-gray-50 min-h-screen">
-      <>
-        <Navbar />
-        <hr />
-        <div className= "flex w-full">
-          <Sidebar />
-          <div className= "w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
-          <Routes>
-            <Route path="/add" element={<Add />} />
-            <Route path="/list" element={<List />} />
-            <Route path="/add" element={<Orders />} />
-          </Routes>
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className= "flex w-full">
+            <Sidebar />
+            <div className= "w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/add" element={<Orders />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </>
+        </>
+      )}
     </div>
   );
 };
