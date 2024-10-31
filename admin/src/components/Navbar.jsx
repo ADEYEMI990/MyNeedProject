3b16ef6efdc7866139dc7d7399889a54deec2ea4
Adeyemi <adeyemiibrahim990@gmail.com>
2024-10-31 03:01:00 -0700

    Making the Logout function perfectly working

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { assets } from "../assets/assets";
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className= "flex items-center py-2 px-[4%] justify-between">
       <div className= "flex flex-col w-[max(10%,80px)] min-w-[80px] justify-center">
@@ -12,7 +12,7 @@ const Navbar = () => {
           ADMIN PANEL
         </span>
       </div>
-      <button className= "bg-gray-600 text-white rounded-full text-xs sm:text-sm px-8 py-2 mt-0 hover:bg-gray-700">
+      <button onClick={()=>setToken('')} className= "bg-gray-600 text-white rounded-full text-xs sm:text-sm px-8 py-2 mt-0 hover:bg-gray-700">
         Logout
       </button>
     </div>
