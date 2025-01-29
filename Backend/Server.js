ba0a0ea91e520724a1b1f8345c2637e98598e61c
Adeyemi <adeyemiibrahim990@gmail.com>
2025-01-29 07:17:57 -0800

    Enable CORS for admin

--- Backend/Server.js
+++ Backend/Server.js
@@ -27,7 +27,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Enable CORS
-const allowedOrigins = ['https://myneed-frontend.vercel.app', 'http://localhost:5173', 'http://localhost:5174'];
+const allowedOrigins = ['https://myneed-frontend.vercel.app','https://myneed-admin.vercel.app', 'http://localhost:5173', 'http://localhost:5174'];
 
 app.use(cors({
   origin: (origin, callback) => {
